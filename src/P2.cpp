7468116516c8e173a93e79d7751433ec059bd851
Kevin Dulanto <kevin.dulanto@utec.edu.pe>
2024-12-04 21:38:05 -0500

    Update

--- src/P2.cpp
+++ src/P2.cpp
@@ -1,7 +1,31 @@
-//
-// Created by rudri on 10/11/2020.
-//
-
 #include <iostream>
 #include "P2.h"
 using namespace std;
+
+template <size_t T, typename Compare, typename Container>
+vector<typename Container::value_type> top_distance(
+        const Container& values,
+        const typename Container::value_type& origin)
+{
+    using Point = typename Container::value_type;
+    using DistanceType = decltype(origin.squared_distance(origin));
+
+    vector<pair<DistanceType, Point>> distances;
+    for (const auto& point : values) {
+        distances.emplace_back(point.squared_distance(origin), point);
+    }
+
+    sort(distances.begin(), distances.end(),
+              [](const auto& a, const auto& b) { return Compare{}(a.first, b.first); });
+
+    vector<Point> result;
+    for (size_t i = 0; i < min(T, distances.size()); ++i) {
+        result.push_back(distances[i].second);
+    }
+
+    return result;
+}
+
+template vector<point<int>> top_distance<4, greater<int>, vector<point<int>>>(const vector<point<int>>&, const point<int>&);
+template vector<point<int>> top_distance<3, less<int>, vector<point<int>>>(const vector<point<int>>&, const point<int>&);
+template vector<point<int>> top_distance<7, greater<int>, vector<point<int>>>(const vector<point<int>>&, const point<int>&);
--- src/P2.h
+++ src/P2.h
@@ -1,8 +1,34 @@
-//
-// Created by rudri on 10/11/2020.
-//
-
 #ifndef PROG3_HEAP_HASH_V2022_1_P2_H
 #define PROG3_HEAP_HASH_V2022_1_P2_H
+
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+struct point {
+    T x, y;
+
+    point(T x = 0, T y = 0) : x(x), y(y) {}
+
+    T squared_distance(const point<T>& other) const {
+        T dx = x - other.x;
+        T dy = y - other.y;
+        return dx * dx + dy * dy;
+    }
+
+    friend ostream& operator<<(std::ostream& os, const point<T>& p) {
+        os << "(" << p.x << ", " << p.y << ")";
+        return os;
+    }
+};
+
+template <size_t T, typename Compare, typename Container>
+vector<typename Container::value_type> top_distance(
+        const Container& values,
+        const typename Container::value_type& origin);
 
 #endif //PROG3_HEAP_HASH_V2022_1_P2_H
